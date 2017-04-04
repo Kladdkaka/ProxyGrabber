089960b5c87ef70024fbc25178811d5a7a4af3f0
Kladdkaka <erik@erikssonwheeler.se>
2017-04-04 13:46:47 +0200

    messy, but added a lot of sources, added a shitty proxy checker, and i also found a source (pike.hqpeak.com) that is a blacklist for proxies with a public api, so i get their ips and search common ports, i got 3000 proxies from just them

---
+++ pike_hqpeak.py
@@ -0,0 +1,48 @@
+import requests
+import json
+from multiprocessing.dummy import Pool as ThreadPool
+
+THREADS = 1000
+TEST_URL = 'http://canihazip.com/s'
+PROXY_PORTS = [8080, 45554, 3128, 1080, 80, 8888]
+
+pool = ThreadPool(THREADS)
+
+r = requests.get('http://pike.hqpeak.com/api/proxy').json()
+proxies = []
+
+for ip in r:
+    for port in PROXY_PORTS:
+        proxies.append(ip + ':' + str(port))
+
+print(proxies)
+
+count = 0
+def check_proxy(proxy):
+    global count
+    count += 1
+    print(count, len(proxies))
+
+    _proxies = {
+        'http': 'http://' + proxy,
+        'https': 'https://' + proxy
+    }
+    try:
+        r = requests.get(TEST_URL, proxies=_proxies, timeout=5)
+        return { 'proxy': proxy, 'working': True }
+    except Exception as e:
+        print(e)
+        return { 'proxy': proxy, 'working': False }
+
+
+results = pool.map(check_proxy, proxies)
+
+working = []
+
+for result in results:
+    if result['working']:
+        working.append(result['proxy'])
+
+with open('working.txt', 'w') as f:
+    for proxy in working:
+        f.write(proxy + '\n')
--- proxy_grabber.py
+++ proxy_grabber.py
@@ -1,10 +1,18 @@
 import re
+import gzip
+import datetime
+import requests
+import json
+
 from xml.dom import minidom
 
-from requests import Session as WebClient
 from pyquery import PyQuery
+from multiprocessing.dummy import Pool as ThreadPool
 
 proxy_regex = re.compile('(?:\d{1,3}\.){3}\d{1,3}:\d+')
+
+pool = ThreadPool(10)
+
 
 class ProxyGrab:
     def __init__(self):
@@ -34,47 +42,73 @@ class ProxyGrab:
             if (names and name in names) or (not names and enabled):
                 print('get proxies from', name, end='\r')
                 try:
-                    site_proxies = set(func(WebClient(), url))
+                    site_proxies = set(func(url))
                 except ValueError:
                     print('URL %s failed' % url)
                     raise
                 proxies.update(site_proxies)
                 print('got %d proxies from %s, now: %d' % (len(site_proxies), name, len(proxies)))
-        return proxies
+        return list(proxies)
 
-def by_linesplit(client, url):
-    return client.get(url).text.splitlines()
+
+def by_linesplit(url):
+    return requests.get(url).text.splitlines()
+
 
 def by_json_dicts(host_key, port_key):
-    def _by_json_dicts(client, url):
-        for data in client.get(url).json():
+    def _by_json_dicts(url):
+        for data in requests.get(url).json():
             yield '%s:%s' % (data[host_key], data[port_key])
     return _by_json_dicts
 
-def by_regex(client, url):
-    return proxy_regex.findall(client.get(url).text)
 
-def by_regex_for_first_link_with_proxies(client, url):
-    response = client.get(url)
+def by_regex(url):
+    r = requests.get(url).text
+    return proxy_regex.findall(r)
+
+
+def by_regex_for_first_link_with_proxies(url):
+    response = requests.get(url)
     for link in PyQuery(response.text, parser='html').items('a'):
         lurl = url + link.attr.href
         print('get proxies from', lurl, end='\r')
-        proxies = set(by_regex(client, lurl))
+        proxies = set(by_regex(lurl))
         print('got %d proxies from %s' % (len(proxies), lurl))
         if proxies:
             return proxies
 
-def by_gziputf8split(client, url):
-    return gzip.decompress(client.get(url).content).decode('utf8').split()
 
-def by_xmldom(client, url):
-    xmldoc = minidom.parseString(client.get(url).text)
+def by_gziputf8split(url):
+    return gzip.decompress(requests.get(url).content).decode('utf8').split()
+
+
+def by_xmldom(url):
+    xmldoc = minidom.parseString(requests.get(url).text)
     for item in xmldoc.getElementsByTagName('prx:proxy'):
         yield '%s:%s' % (item.getElementsByTagName('prx:ip')[0].firstChild.nodeValue,
                          item.getElementsByTagName('prx:port')[0].firstChild.nodeValue)
 
 
+def by_checkerproxy(baseurl):
+    urls = []
+    dates = []
+    proxies = []
+
+    for days in range(0, 7):
+        dates.append(datetime.datetime.now() - datetime.timedelta(days=days))
+
+    for date in dates:
+        urls.append(baseurl + date.strftime('%Y-%m-%d'))
+
+    results = pool.map(by_regex, urls)
+
+    for proxy_list in results:
+        proxies.extend(proxy_list)
+
+    return list(set(proxies))
+
 grab = ProxyGrab()
+
 grab.hidester = 'https://hidester.com/..', by_json_dicts('IP', 'PORT'), 0  # filters, pagination and json, not now
 grab.free_proxy_list = 'http://free-proxy-list.appspot.com/proxy.json', by_json_dicts('Host', 'Port')
 grab.xicidaili = 'http://api.xicidaili.com/free2016.txt', by_linesplit
@@ -88,19 +122,8 @@ grab.rosinstrument1 = 'http://tools.rosinstrument.com/proxy/plab100.xml', by_reg
 grab.rosinstrument2 = 'http://tools.rosinstrument.com/proxy/l100.xml', by_regex
 grab.xroxy = 'http://www.xroxy.com/proxyrss.xml', by_xmldom
 grab.orcatech = 'https://orca.tech/community-proxy-list/', by_regex_for_first_link_with_proxies
+grab.checkerproxy = 'https://checkerproxy.net/api/archive/', by_checkerproxy
+grab.freevpn_ninja = 'https://freevpn.ninja/free-proxy/txt', by_regex
 
-
-# grab.orcatech = 'https://orca.tech/?action=real-time-proxy-list', by_orcatech_html, 0  # dead
-
-#=======================================================================================================================
-# def by_orcatech_html(client, url):
-#     fstr, tstr = '<button class="btn" data-clipboard-text="', '"><i class="mdi-content-content-copy left">'
-#     html = client.get(url).text
-#     for line in html[html.index(fstr) + len(fstr):html.index(tstr)].splitlines():
-#         proxy = ''
-#         for char in line:
-#             if char in '0123456789:.':
-#                 proxy += char
-#         if proxy:
-#             yield proxy
-#=======================================================================================================================
+with open('proxies.json', 'w') as f:
+    f.write(json.dumps(grab.get_proxies()))
