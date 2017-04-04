f491f8d2a5fe5ac1accb0624a622c2f92d95b2e3
Kladdkaka <Kladdkaka@users.noreply.github.com>
2017-04-04 10:16:48 +0200

    Update proxy_grabber.py

--- proxy_grabber.py
+++ proxy_grabber.py
@@ -1,197 +1,106 @@
-import requests
 import re
-import gzip
 from xml.dom import minidom
+
+from requests import Session as WebClient
+from pyquery import PyQuery
 
 proxy_regex = re.compile('(?:\d{1,3}\.){3}\d{1,3}:\d+')
 
+class ProxyGrab:
+    def __init__(self):
+        self._sites = {}
 
-def flatten(l):
-    a = []
+    def __getattr__(self, name):
+        try:
+            return self._sites[name.upper()]
+        except KeyError:
+            raise AttributeError(name)
 
-    for x in l:
-        for z in x:
-            a.append(z)
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            return super().__setattr__(name, value)
+        value = value if len(value) == 3 else value + (True,)
+        self._sites[name.upper()] = value
 
-    return a
+    def __iter__(self):
+        return iter((n,) + vs for n, vs in self._sites.items())
 
+    def __dir__(self):
+        return super().__dir__() + list(self._sites)
 
-def hidester(): # broken
-    r = requests.get('https://hidester.com/proxydata/php/data.php?mykey=csv&gproxy=2')
+    def get_proxies(self, *names):
+        proxies, names = set(), {n.upper() for n in names}
+        for name, url, func, enabled in self:
+            if (names and name in names) or (not names and enabled):
+                print('get proxies from', name, end='\r')
+                try:
+                    site_proxies = set(func(WebClient(), url))
+                except ValueError:
+                    print('URL %s failed' % url)
+                    raise
+                proxies.update(site_proxies)
+                print('got %d proxies from %s, now: %d' % (len(site_proxies), name, len(proxies)))
+        return proxies
 
-    proxies = []
+def by_linesplit(client, url):
+    return client.get(url).text.splitlines()
 
-    for proxy_data in r.json():
-        proxies.append('{0}:{1}'.format(proxy_data['IP'], proxy_data['PORT']))
+def by_json_dicts(host_key, port_key):
+    def _by_json_dicts(client, url):
+        for data in client.get(url).json():
+            yield '%s:%s' % (data[host_key], data[port_key])
+    return _by_json_dicts
 
-    return list(set(proxies))
+def by_regex(client, url):
+    return proxy_regex.findall(client.get(url).text)
 
+def by_regex_for_first_link_with_proxies(client, url):
+    response = client.get(url)
+    for link in PyQuery(response.text, parser='html').items('a'):
+        lurl = url + link.attr.href
+        print('get proxies from', lurl, end='\r')
+        proxies = set(by_regex(client, lurl))
+        print('got %d proxies from %s' % (len(proxies), lurl))
+        if proxies:
+            return proxies
 
-def free_proxy_list():
-    r = requests.get('http://free-proxy-list.appspot.com/proxy.json')
+def by_gziputf8split(client, url):
+    return gzip.decompress(client.get(url).content).decode('utf8').split()
 
-    proxies = []
-
-    for proxy_data in r.json():
-        proxies.append('{0}:{1}'.format(proxy_data['Host'], proxy_data['Port']))
-
-    return list(set(proxies))
-
-
-def xicidaili():
-    r = requests.get('http://api.xicidaili.com/free2016.txt')
-
-    proxies = r.text.splitlines()
-
-    return list(set(proxies))
-
-
-def orcatech(): # broken
-    r = requests.get('https://orca.tech/?action=real-time-proxy-list')
-
-    proxies = []
-    allowed_chars = '0123456789:.'
-
-    html = r.text
-
-    raw_proxies = html[html.index('<button class="btn" data-clipboard-text="') + len(
-        '<button class="btn" data-clipboard-text="'):html.index('"><i class="mdi-content-content-copy left">')]
-
-    for line in raw_proxies.splitlines():
-        proxy = ''
-
-        for char in line:
-            if char in allowed_chars:
-                proxy += char
-
-        proxies.append(proxy)
-
-    return list(set(proxies))
-
-
-def proxyape():
-    r = requests.get('http://proxyape.com/')
-
-    proxies = []
-
-    html = r.text
-
-    proxies = proxy_regex.findall(html)
-
-    return list(set(proxies))
-
-
-def proxyspy():
-    r = requests.get('http://txt.proxyspy.net/proxy.txt')
-
-    proxies = []
-
-    html = r.text
-
-    proxies = proxy_regex.findall(html)
-
-    return list(set(proxies))
-
-
-def cn_66ip():
-    r = requests.get('http://www.66ip.cn/mo.php?tqsl=1000000')
-
-    proxies = []
-
-    html = r.text
-
-    proxies = proxy_regex.findall(html)
-
-    return list(set(proxies))
-
-
-def proxyrss():
-    r = requests.get('http://www.proxyrss.com/proxylists/all.gz')
-
-    return list(set(gzip.decompress(r.content).decode('utf8').split()))
-
-
-def proxylists():
-    r = requests.get('http://www.proxylists.net/proxylists.xml')
-
-    proxies = []
-
-    xmldoc = minidom.parseString(r.text)
-
+def by_xmldom(client, url):
+    xmldoc = minidom.parseString(client.get(url).text)
     for item in xmldoc.getElementsByTagName('prx:proxy'):
-        proxies.append(
-            item.getElementsByTagName('prx:ip')[0].firstChild.nodeValue + ':' + item.getElementsByTagName('prx:port')[
-                0].firstChild.nodeValue)
-
-    return list(set(proxies))
+        yield '%s:%s' % (item.getElementsByTagName('prx:ip')[0].firstChild.nodeValue,
+                         item.getElementsByTagName('prx:port')[0].firstChild.nodeValue)
 
 
-def proxz():
-    r = requests.get('http://www.proxz.com/proxylists.xml')
-
-    proxies = []
-
-    xmldoc = minidom.parseString(r.text)
-
-    for item in xmldoc.getElementsByTagName('prx:proxy'):
-        proxies.append(
-            item.getElementsByTagName('prx:ip')[0].firstChild.nodeValue + ':' + item.getElementsByTagName('prx:port')[
-                0].firstChild.nodeValue)
-
-    return list(set(proxies))
-
-
-def rosinstrument():
-    r1 = requests.get('http://tools.rosinstrument.com/proxy/plab100.xml')
-    r2 = requests.get('http://tools.rosinstrument.com/proxy/l100.xml')
-
-    xmlraw1 = r1.text
-    xmlraw2 = r2.text
-
-    proxies1 = proxy_regex.findall(xmlraw1)
-    proxies2 = proxy_regex.findall(xmlraw2)
-
-    proxies = list(set(proxies1 + proxies2))
-
-    return proxies
+grab = ProxyGrab()
+grab.hidester = 'https://hidester.com/..', by_json_dicts('IP', 'PORT'), 0  # filters, pagination and json, not now
+grab.free_proxy_list = 'http://free-proxy-list.appspot.com/proxy.json', by_json_dicts('Host', 'Port')
+grab.xicidaili = 'http://api.xicidaili.com/free2016.txt', by_linesplit
+grab.proxyape = 'http://proxyape.com/', by_regex
+grab.proxyspy = 'http://txt.proxyspy.net/proxy.txt', by_regex, 0  # dunno, takes too long
+grab.cn_66ip = 'http://www.66ip.cn/mo.php?tqsl=1000000', by_regex
+grab.proxyrss = 'http://www.proxyrss.com/proxylists/all.gz', by_gziputf8split
+grab.proxylists = 'http://www.proxylists.net/proxylists.xml', by_xmldom
+grab.proxz = 'http://www.proxz.com/proxylists.xml', by_xmldom
+grab.rosinstrument1 = 'http://tools.rosinstrument.com/proxy/plab100.xml', by_regex
+grab.rosinstrument2 = 'http://tools.rosinstrument.com/proxy/l100.xml', by_regex
+grab.xroxy = 'http://www.xroxy.com/proxyrss.xml', by_xmldom
+grab.orcatech = 'https://orca.tech/community-proxy-list/', by_regex_for_first_link_with_proxies
 
 
-def xroxy():
-    r = requests.get('http://www.xroxy.com/proxyrss.xml')
+# grab.orcatech = 'https://orca.tech/?action=real-time-proxy-list', by_orcatech_html, 0  # dead
 
-    proxies = []
-
-    xmldoc = minidom.parseString(r.text)
-
-    for item in xmldoc.getElementsByTagName('prx:proxy'):
-        proxies.append(
-            item.getElementsByTagName('prx:ip')[0].firstChild.nodeValue + ':' + item.getElementsByTagName('prx:port')[
-                0].firstChild.nodeValue)
-
-    return list(set(proxies))
-
-
-_proxies = flatten([
-    #hidester(),
-    free_proxy_list(),
-    xicidaili(),
-    #orcatech(),
-    proxyape(),
-    proxyspy(),
-    cn_66ip(),
-    proxyrss(),
-    proxylists(),
-    proxz(),
-    rosinstrument(),
-    xroxy()
-])
-
-print(len(_proxies))
-print(len(list(set(_proxies))))
-
-with open('proxies.txt', 'w') as f:
-    f.write(
-        '\n'.join(
-            list(set(_proxies))
-        )
-    )
+#=======================================================================================================================
+# def by_orcatech_html(client, url):
+#     fstr, tstr = '<button class="btn" data-clipboard-text="', '"><i class="mdi-content-content-copy left">'
+#     html = client.get(url).text
+#     for line in html[html.index(fstr) + len(fstr):html.index(tstr)].splitlines():
+#         proxy = ''
+#         for char in line:
+#             if char in '0123456789:.':
+#                 proxy += char
+#         if proxy:
+#             yield proxy
+#=======================================================================================================================
